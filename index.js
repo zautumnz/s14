b8daa5d861914b1e7e584b731ad9852467162284
zautumnz <shitianyan@pm.me>
2025-11-01 21:18:05 -0700

    feat: do it

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('coming soon')
+import { readdirSync } from 'node:fs'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import t from 'terminal-image'
+
+const dir = 'emotes'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const imgs = readdirSync(resolve(__dirname, dir))
+const rand = imgs[Math.floor(Math.random() * imgs.length)]
+const p = await t.file(resolve(dir, rand))
+console.log(p)
